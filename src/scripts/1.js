1ed4b1d32c05b791a9857ebc8f8716e2a3ea9720
juancarloselorriaga <juancarloselorriaga@gmail.com>
2018-10-16 22:11:22 -0500

    appfairy: Migrate

---
+++ src/scripts/1.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+(function() {
+
+WebFont.load({google:{families:["Muli:200,300,regular,600,700,800,900"]}});
+
+}).call(window)
+
+/* eslint-enable */
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,49 +1,7 @@
 /* eslint-disable */
 
-const styles = [
-  {
-    type: 'href',
-    body: '/css/normalize.css',
-  },
-  {
-    type: 'href',
-    body: '/css/webflow.css',
-  },
-  {
-    type: 'href',
-    body: '/css/heimerweb.webflow.css',
-  },
-]
-
-const loadingStyles = styles.map((style) => {
-  let styleEl
-  let loading
-
-  if (style.type == 'href') {
-    styleEl = document.createElement('link')
-
-    loading = new Promise((resolve, reject) => {
-      styleEl.onload = resolve
-      styleEl.onerror = reject
-    })
-
-    styleEl.rel = 'stylesheet'
-    styleEl.type = 'text/css'
-    styleEl.href = style.body
-  }
-  else {
-    styleEl = document.createElement('style')
-    styleEl.type = 'text/css'
-    styleEl.innerHTML = style.body
-
-    loading = Promise.resolve()
-  }
-
-  document.head.appendChild(styleEl)
-
-  return loading
-})
-
-module.exports = Promise.all(loadingStyles)
+require('./0.css')
+require('./1.css')
+require('./2.css')
 
 /* eslint-enable */
